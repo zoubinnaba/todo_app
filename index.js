5e30408c9f4a24b691536cb45ccebef14a9fc35a
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2022-06-28 14:30:49 +0000

    add button

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 let todos = []
 
-const inputEl = document.getElementById("input-el");
+const inputEl = document.getElementById("input-el")
 
-const addTask = document.getElementById("addTask-btn");
+const addTask = document.getElementById("addTask-btn")
 
-const listOfTodo = document.getElementById("liste-of-todos");
+const listOfTodo = document.getElementById("liste-of-todos")
 
-const createTodo = () => {
-    let todo = inputEl.value
-    todos.push(todo)
-    inputEl.value = ""
-    console.log(todo)
-    return listOfTodo.innerHTML += `<li class="todo-el">${todo}</li>`
+const completeBtn = document.getElementById("complete-btn")
+
+const todoEl = document.getElementById("todo-el")
+
+const renderTodo = (todo) => {
+    let todoItems = ''
+    for(let i = 0; i < todo.length; i++) {
+        //todos[i] = todos.push(todo)
+        todoItems += `
+            <li class="todo-el" id="todo-el">
+                ${todo[i]}
+                <button id='complete-btn'>Complete Task</button>
+            </li>
+            `
+        //console.log(todos.push(todos[i]))
+    }
+    listOfTodo.innerHTML = todoItems
 }
 
 addTask.addEventListener('click', function() {
-    createTodo()
+    todos.push(inputEl.value)
+    inputEl.value = ""
+    renderTodo(todos)
+})
+
+
+completeBtn.addEventListener('click', function() {
+    console.log("complete")
+    //todoEl.style.textDecoration = overline
 })
